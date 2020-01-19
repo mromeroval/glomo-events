67cb6c71c2e28f40c7d1df94b6f3001e0832c56c
mromeroval <mauricio.romero@outlook.com>
2020-01-19 12:49:31 +0100

    Enable CORS

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const path = require('path');
+var cors = require('cors')
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Enable All CORS Requests just for this test
+app.use(cors())
 
 // Body Parser Middleware
 app.use(express.json());
