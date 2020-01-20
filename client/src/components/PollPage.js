2ab230e5461cd1639c859b43950b1e370d907f91
mromeroval <mauricio.romero@outlook.com>
2020-01-21 00:25:45 +0100

    Implement frontend functions for creating random events

--- client/src/components/PollPage.js
+++ client/src/components/PollPage.js
@@ -9,36 +9,90 @@ class PollList extends React.Component {
     this.state = {
       isLoading: false,
       events: [],
+      activeEvents: [],
+      activeCategory: "",
+      categories: [],
       activeEvent: {}
     };
   }
 
-   getSportList = (sport) => {
-    const events = this.state.events;
-    const sportList = [];
-    events.map(event => {
-      if (event.sport === sport) {
-        sportList.push(event)
-      }
-      return sportList
+  // Get list of sports by category
+  //  getSportList = (sport) => {
+  //   const events = this.state.events;
+  //   const sportList = [];
+  //   events.map(event => {
+  //     if (event.sport === sport) {
+  //       sportList.push(event)
+  //     }
+  //     return sportList
+  //   })
+  // }
+
+  // Set list of sport categories
+   setCategories = (events) => {
+    const categories = [];
+    events.forEach(event => {
+        categories.push(event.sport)
     })
+    let unique = [...new Set(categories)];
+    this.setState({categories:unique})
   }
 
-  getEvent = (eventId) => {
-    const events = this.state.events;
-    const event = events.filter(event => event.id === eventId)
-    this.setState({activeEvent: event})
-      return event
+  // Return random sport category
+  randomSport = (sports) => {
+    return sports[Math.floor(Math.random() * sports.length)];
+ }
+
+  // Set random event from active events
+  setRandomEvent = () => {
+    let events = this.state.activeEvents;
+    console.log("Actives: ",events)
+    let randomEvent = events[Math.floor(Math.random() * events.length)];
+    let remain = events.filter(event => event.id !== randomEvent.id)
+    console.log("Random:",randomEvent)
+    this.setState({activeEvent:randomEvent});
+    this.setState({activeEvents : remain});
+    console.log("Actives remain: ",events)
+ }
+
+  // Set active events
+  setActiveEvents = (events) => {
+    let sport = this.randomSport(this.state.categories)
+    this.setState({activeCategory:sport})
+    let activeEvents = events.filter(event => event.sport === sport)
+    this.setState({activeEvents: activeEvents})
   }
 
-  setActiveEvent = (event) => {
-    this.setState({activeEvent: event})
-  }
+  // Get categories
+  // getSportCategories = () => {
+  //   const events = this.state.events;
+  //   const categories= [];
+  //   events.map(event => {
+  //     if (event.sport === sport) {
+  //       sportList.push(event)
+  //     }
+  //     return categories
+  //   })
+  // }
 
+  // Get event by ID
+  // getEvent = (eventId) => {
+  //   const events = this.state.events;
+  //   const event = events.filter(event => event.id === eventId)
+  //   this.setState({activeEvent: event})
+  //     return event
+  // }
+
+  // Set active event
+  // setActiveEvent = (event) => {
+  //   this.setState({activeEvent: event})
+  // }
+
+  // Post event vote
   sendVote = (vote) => {
     const data= {
       event: this.state.activeEvent.id,
-      user: "user4@mail.com",
+      user: "user@mail.com",
       homeName: 0,
       awayName: 0,
       draw: 0
@@ -52,9 +106,12 @@ class PollList extends React.Component {
       body: JSON.stringify(data)
     })
     .then( response => {
-      if(!response.ok){ throw new Error('Network response was Not OK')}
+      if (!response.ok) {
+        throw new Error('Network response was Not OK')
       }
+    }
     )
+    .then(this.setRandomEvent())
     .catch((error) => {
       console.error( error);
     })
@@ -71,24 +128,27 @@ class PollList extends React.Component {
         return response.json()
       })
       .then(events => {
-        this.setState({
-          events,
-          activeEvent : events[0]
-        });
+        this.setState({events});
+        this.setCategories(events);
+        this.setActiveEvents(events);
+        this.setRandomEvent();
+        // this.setState({activeEvent:this.state.events[1]})
+        console.log(this.state)
       })
       .then(this.setState({ isLoading:false }))
       .catch((error) => {
         console.error('There has been a problem with your fetch operation:', error);
       })
   }
+  
 
   render() {
     return (
       <div>
+        
         <PollForm
           isLoading = {this.state.isLoading}
           event = {this.state.activeEvent}
-          events = {this.state.events}
           sendVote = {this.sendVote}
         />
       </div>
